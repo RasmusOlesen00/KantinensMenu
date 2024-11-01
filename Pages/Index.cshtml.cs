3d30ded06e011a6fc94034f63a97fbbd07253b6b
RasmusOlesen00 <146640318+RasmusOlesen00@users.noreply.github.com>
2024-11-01 12:01:05 +0100

    menu db

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,49 @@
+using Azure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Azure.Data.Tables;
+using IBAS_kantine.Model;
+using System.Collections.Generic;
+using Azure.Storage.Blobs;
 
-namespace IBAS_kantine.Pages;
-
-public class IndexModel : PageModel
+namespace IBAS_kantine.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
 
-    public void OnGet()
-    {
+        public List<MenuItemDTO> MenuItems { get; set; } 
 
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            MenuItems = new List<MenuItemDTO>(); 
+        }
+
+        public void OnGet()
+        {
+            // get Blob connection string
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGEBLOB_CONNECTIONSTRING");
+
+            // Create a BlobServiceClient object 
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            var tableName = "Menu2";
+           
+            TableClient tableClient = new TableClient(connectionString, tableName);
+
+            Pageable<TableEntity> entities = tableClient.Query<TableEntity>();
+
+            foreach (TableEntity entity in entities)
+            {
+                var dto = new MenuItemDTO
+                {
+                    Dag = entity.RowKey,
+                    KoldRet = entity.GetString("KoldRet"),
+                    VarmRet = entity.GetString("VarmRet"),
+                    Uge = Int32.Parse(entity.PartitionKey)
+                };
+                MenuItems.Add(dto); 
+            }
+        }
     }
 }
